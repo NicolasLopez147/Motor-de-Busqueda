6c4f6ff39bfe4407b8fe867ea7b4a30904362b7e
nicolaslopez99 <lopeznicolas3719@gmail.com>
2020-11-16 14:14:59 -0500

    main.py

--- main.py
+++ main.py
@@ -4,3 +4,25 @@ documento = open("EnfrentandolaEspadadeDobleFilo.pdf")
 print(dir(documento))
 print(help(documento.readline))
 print(type(documento.read(5)))
+def pintar(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            if j == len(matriz)-1:
+                print(matriz[i][j],end = "")
+            else:
+                print(matriz[i][j],end = " ")
+        print()
+
+n = int(input())
+numeros = input().split(" ")
+numeros = list(map(int,numeros))
+tablero = []
+for i in range(n):
+    tablero.append([0 for j in range(n)])
+for i in range(n):
+    for j in range(n):
+        m = max(abs(numeros[0]-i),abs(numeros[1]-j))
+        if numeros[2] - m > 0:
+            tablero[i][j] = numeros[2]-m
+
+pintar(tablero)
