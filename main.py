f9f15fe3a5d721bed330af5a4e14c2357c6ca027
NicolasLopez147 <74556776+NicolasLopez147@users.noreply.github.com>
2020-11-20 13:59:49 -0500

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import PyPDF2
-documento = open("EnfrentandolaEspadadeDobleFilo.pdf")
-#leer = PyPDF2.PdfFileReader(documento)
-print(dir(documento))
-print(help(documento.readline))
-print(type(documento.read(5)))
