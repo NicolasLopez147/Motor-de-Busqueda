78aecc700423ad4dbc0f0c124a037d9a2d714920
Julian Andres Cordoba Jaramillo <jucoroba@unal.edu.co>
2020-11-21 15:51:51 -0500

    funcion txt

--- leer.py
+++ leer.py
@@ -11,10 +11,20 @@ def leer_PDF(arg):
     read_pdf = pypdf2.PdfFileReader(pdf_file)
     number_of_pages = read_pdf.getNumPages()
     contenido = ""
-    for i in range(number_of_pages):
+    for i in range(number_of_pages - 1):
         page = read_pdf.getPage(i)
         page_content = page.extractText()
         contenido = contenido + page_content
+    contenido.split()
+    contenido = set(contenido)
+    return(contenido)
+
+def leer_TXT(arg):
+    archivo = open(arg, "r")
+    contenido = ""
+    for linea in archivo.readlines():
+        contenido = contenido + linea
+    archivo.close() 
     contenido.split()
     contenido = set(contenido)
     return(contenido)
