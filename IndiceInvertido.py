dc49a3ea335b6f527c9e15eda1d4d15e73b66c60
NicolasLopez147 <nilopezn@unal.edu.co>
2020-11-30 21:53:54 -0500

    all

--- IndiceInvertido.py
+++ IndiceInvertido.py
@@ -1,19 +1,35 @@
-import os
+import os.path
 import leer
 import glob
 from os.path import isdir,join
+import pickle
+from os import startfile
 def buscarIndice(path,pbuscar):
 	di = os.listdir(path)
 	if "index.pickle" in di:
 		print("El indice ya existe")
-		buscar(pbuscar)
+		buscar(path,pbuscar)
 	else:
 		print("El indice sera creado")
 		crear(path)
-		buscar(pbuscar)
+		buscar(path,pbuscar)
 
-def buscar(pbuscar):
-	pass
+def buscar(path,pbuscar):
+	fpath = join(path,"index.pickle")
+	fichero = open(fpath,'rb')
+	iInvertido = pickle.load(fichero)
+	fichero.close()
+	
+	archivos_encontrados = set()
+	for p in pbuscar:
+		if p in iInvertido.keys():
+			archivos_encontrados.update(iInvertido[p])
+		else:
+			print("No se encontro ninguna referencia a la palabra '{palabra}'".format(palabra=p))
+	archivos_encontrados = list(archivos_encontrados)
+	parchivo = join(path,archivos_encontrados[1])
+	os.startfile(parchivo)
+
 def crear(path):
 	di = os.listdir(path)
 	archivostxt = glob.glob(join(path,"*.txt"))
@@ -22,14 +38,17 @@ def crear(path):
 	iInvertido = {}
 	for atxt in archivostxt:
 		llaves.update(leer.preparar(atxt,"txt"))
+		archivo = str(os.path.split(atxt)[1])
+		print(archivo)
 		for l in llaves:
 			if l not in iInvertido.keys():
-				iInvertido[l] = {atxt}
+				iInvertido[l] = {archivo}
 			else:
-				iInvertido[l].add(atxt)
-	for k,v in iInvertido.items():
-		print(k,":")
-		print(v)
+				iInvertido[l].add(archivo)
+	fpath = join(path,"index.pickle")
+	fichero = open(fpath,"wb")
+	pickle.dump(iInvertido,fichero)
+	fichero.close()
 def comprobar(path,pbuscar):
 	if isdir(path):
 		print("El directorio si existe")
